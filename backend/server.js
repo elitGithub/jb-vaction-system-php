926f99e5b33d0be3c9ac50be1e0aed238ba4a1e4
Eli Tokar <52536795+elitGithub@users.noreply.github.com>
2021-12-25 21:07:26 +0200

    practice

--- backend/server.js
+++ backend/server.js
@@ -7,29 +7,21 @@ const logEvents = require('./logEvents');
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../build/')));
-app.get('/', (req, res) => {
+
+app.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile('index.html');
 });
 
-app.post('/login', (req, res) => {
+app.get('/vacation-list(.html)?', (req, res) => {
+    res.sendFile('index.html');
+});
+
+app.post('/login(.html)?', (req, res) => {
     logEvents.emit('log', `Example app listening at http://localhost:${port}`);
     res.send(JSON.stringify({'success': true, 'message': 'logged in'}));
-
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
 
-/**
- * mongodb root pass: VHeAHDLVqE7dteEw
- * user root
- *
- *
- *  todo:
- *
- *  set up the server and the connection to db
- *  set up a basic crud operation setup for the server
- *  set up the auth and users db
- *  connect the front to the backend.
- */
